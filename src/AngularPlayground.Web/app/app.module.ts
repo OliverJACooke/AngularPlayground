3b6ba7107bfae6a16d65be79420a1c49958ad572
Oliver Cooke <ocooke@hotmail.com>
2017-11-21 20:06:43 +0000

    first commit

---
+++ src/AngularPlayground.Web/app/app.module.ts
@@ -0,0 +1,31 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent }  from "./app.component";
+import { BackgroundVideoComponent } from "./home/background-video.component";
+import { SectionOneComponent } from "./home/section-one.component";
+import { PricesComponent } from "./home/prices.component";
+import { AboutComponent } from "./home/about.component";
+import { ServicesComponent } from "./home/services.component";
+import { TestimonialComponent } from "./home/testimonial.component";
+import { ContactComponent } from "./home/contact.component";
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent,
+        BackgroundVideoComponent,
+        SectionOneComponent,
+        PricesComponent,
+        AboutComponent,
+        ServicesComponent,
+        TestimonialComponent,
+        ContactComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule { }
